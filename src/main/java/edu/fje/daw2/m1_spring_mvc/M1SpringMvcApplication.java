815770e4ec54c6304e59e3b484632c3d44bcd91d
sergigrau <sergi.grau@fje.edu>
2021-02-25 11:24:33 +0100

    Initial commit

---
+++ src/main/java/edu/fje/daw2/m1_spring_mvc/M1SpringMvcApplication.java
@@ -0,0 +1,18 @@
+package edu.fje.daw2.m1_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal de un projecte de Spring MVC
+ * @author sergi.grau@fje.edu
+ * @version 1.0 24.02.21
+ */
+@SpringBootApplication
+public class M1SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1SpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fje/daw2/m1_spring_mvc/M1SpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.fje.daw2.m1_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class M1SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
